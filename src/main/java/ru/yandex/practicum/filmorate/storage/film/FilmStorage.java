8889f3d3261e0b5091f387ebd90b1fbc843a7b8c
Makar-FP <makarka00999@yandex.ru>
2025-02-20 18:18:30 +0300

    Mission complete 4

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -10,10 +10,16 @@ import java.util.List;
 public interface FilmStorage extends Storage<Film> {
 
     public boolean setLikeFilm(long filmId, long userId);
+
     public Film removeLikeFilm(long filmId, long userId);
+
     public List<Film> getPopularFilms(int count);
+
     public List<Genre> getAllGenres();
+
     public Genre getGenreById(int id);
+
     public List<Mpa> findAllMpa();
+
     public Mpa findMpa(int id);
 }
